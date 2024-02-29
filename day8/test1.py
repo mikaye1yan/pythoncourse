69e1778806b552f4c5b44abdf53f8efeb7d1ed3e
mikaye1yan <tsaghik.m@tumo.org>
2024-02-29 22:06:50 +0400

    new

---
+++ day8/test1.py
@@ -0,0 +1,8 @@
+def  func(name,n):
+   if n==1:
+      return f"Hello {name.capitalize()}"
+   else: 
+      return f"Bye {name.capitalize()}"
+# print(func("Tsaghik",0))
+print(func("Tsaghik",1))
+   
