75d347a584cb3237a93be0fdba8822438b77e955
mikaye1yan <tsaghik.m@tumo.org>
2024-03-26 14:26:47 +0400

    new

--- day12/test2.py
+++ day12/test2.py
@@ -1,6 +1,8 @@
 def consecutive_combo(lst1, lst2):
-    comb = sorted(lst1 + lst2)
-    return all(comb[i] == comb[i-1] + 1 for i in range(1, len(comb)))
+    lst1.extend(lst2)   
+    lst1.sort()         
+    return all(lst1[i] == lst1[i-1] + 1 for i in range(1, len(lst1)))
+
 print(consecutive_combo([7, 4, 5, 1], [2, 3, 6])) 
 print(consecutive_combo([1, 4, 6, 5], [2, 7, 8, 9]))  
 print(consecutive_combo([1, 4, 5, 6], [2, 3, 7, 8, 10]))  
